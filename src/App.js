ffa7aff41ed650b5adcf30dfaf222460c10d870d
Dharaneesh Venkatesh <dharaneesh.venkatesh@axonista.com>
2023-02-01 22:54:13 +0000

    Quote Machine base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import TwitterLogo from './Twitter';
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+  const key = process.env.REACT_APP_QUOTE_KEY;
+
+  useEffect(() => {
+    return () => {
+      fetchQuote();
+    };
+  }, []); //Run only on pageLoad
+
+  const fetchQuote = async () => {
+    await fetch('https://api.api-ninjas.com/v1/quotes', {
+      method: 'GET',
+      headers: {
+        'X-API-Key': key,
+      }
+    }).then((response) => response.json())
+    .then((data) => {
+      setQuote(data[0].quote);
+      setAuthor(data[0].author);
+    });
+
+    setRed(Math.floor((Math.random()%255)*100));
+    setGreen(Math.floor((Math.random()%255)*100));
+    setBlue(Math.floor((Math.random()%255)*100));
+    document.getElementById('quote').classList.remove('fadeIn');
+    document.getElementById('author').classList.remove('fadeIn');
+    void document.getElementById('quote').offsetWidth;
+    void document.getElementById('author').offsetWidth;
+    document.getElementById('quote').classList.add('fadeIn');
+    document.getElementById('author').classList.add('fadeIn');
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="quote-box" class="d-flex align-items-center justify-content-center mx-auto" style={{"height": '50em', width:'100%', background: `rgba(${red}, ${green}, ${blue})`}}>
+        <div id="text" class="card w-75 h-auto p-2 border rounded">
+          <span id="quote" class="mt-5 fs-4 fw-normal">
+          {quote ? quote : ''}
+          </span>
+          <br />
+          <div class="d-flex justify-content-end">
+            <span id="author" class="w-25 fs-5 fw-light">{author? author : ''}</span>
+          </div>
+          <br />
+          <div class="text-center">
+            <button class="btn w-25" type="submit" id="new-quote" onClick={fetchQuote} style={{'background': `rgba(${red}, ${green}, ${blue})`, color: 'white'}}>
+              Next Quote
+            </button>
+            <span id="tweet-btn" class="m-2">
+              <a id="tweet-quote" target="_blank" rel="noreferrer" href="https://twitter.com/intent/tweet">
+                <TwitterLogo className="border rounded p-1" style={{'width': '2.5rem', 'height': '2.5rem', 'fill': 'white', background: `rgba(${red}, ${green}, ${blue})`}}/>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
